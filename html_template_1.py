88bdd0badf2dad5a2a9f2933189b343030eb5024
Tomas Baron <tomas.barong@gmail.com>
2025-03-21 11:54:56 -0500

    Update html_template_1.py

--- html_template_1.py
+++ html_template_1.py
@@ -27,6 +27,6 @@ css = '''
 
 logo = '''
 <div style= margin-bottom: 15px;">
-    <img src="https://i.ibb.co/cgQbQTQ/logo.png" alt="Logo" style="max-width: 20%; height: 20%;">
+    <img src="https://imgur.com/a/5KCFsvQ" alt="Logo" style="max-width: 20%; height: 20%;">
 </div>
 '''
